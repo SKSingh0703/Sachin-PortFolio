5239dc99a691914387083a24fd7852de6eb86558
Sachin Kumar <157154133+SKSingh0703@users.noreply.github.com>
2024-07-13 09:54:58 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,29 @@
+// const express = require('express');
+// const app = express();
+// require("dotenv").config();
+// const dbconfig = require("./config/dbconfig"); // Corrected spelling
 
+// const portfolioRoute = require("./routes/portfolioRoute"); // Corrected spelling
+
+// app.use(express.json());
+
+// app.use("/api/portfolio", portfolioRoute);
+
+// const port = process.env.PORT || 10000  ;
+// app.listen(port, () => {
+//     console.log(`Server Listening on ${port}`);
+// });
 const express = require('express');
 const app = express();
 require("dotenv").config();
-const dbconfig = require("./config/dbconfig"); // Corrected spelling
-
-const portfolioRoute = require("./routes/portfolioRoute"); // Corrected spelling
+const dbconfig = require("./config/dbconfig");
+const portfolioRoute = require("./routes/portfolioRoute");
 
 app.use(express.json());
-
 app.use("/api/portfolio", portfolioRoute);
 
-const port = process.env.PORT || 10000  ;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server Listening on ${port}`);
 });
+
