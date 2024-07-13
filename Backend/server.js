8e2c158a33030c14305423a6485ab4b78f80e4e7
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-07-13 06:42:23 +0530

    lkj

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const app = express();
+require("dotenv").config();
+const dbconfig = require("./config/dbconfig"); // Corrected spelling
+
+const portfolioRoute = require("./routes/portfolioRoute"); // Corrected spelling
+
+app.use(express.json());
+
+app.use("/api/portfolio", portfolioRoute);
+
+
+// const path = require("path");
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname,"client/build")));
+//     app.get("*",(req,res) => {
+//         res.sendFile(path.join(__dirname,"client/build/index.html"));
+//     });
+// }
+const port = process.env.PORT || 5000  ;
+app.listen(port, () => {
+    console.log(`Server Listening on ${port}`);
+});
