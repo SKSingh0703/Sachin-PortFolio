3f9976e92b57454fc8a3764787ac9a0c4bd77502
Sachin Kumar <157154133+SKSingh0703@users.noreply.github.com>
2024-07-13 16:48:18 +0530

    Update Education.js

--- client/src/pages/Home/Education.js
+++ client/src/pages/Home/Education.js
@@ -1,39 +1,33 @@
-import React from 'react'
-import SectionTitle from '../../components/SectionTitle'
+import React from 'react';
+import SectionTitle from '../../components/SectionTitle';
 
 function Education() {
   return (
-    <div >
-        <SectionTitle title="Education" />
-        <div className='flex flex-col text-white gap-10 py-10 justify-center '>
-        <p>
-            <b>Birla Institute of Technology</b> 
-            <br></br>
-            B.Tech in CSE CGPA-9.04
-            <br></br>
-            September 2024-2027
-        </p>
-        <p>  
-          Jee Main <b>AIR : 2440</b> 
-         </p>
-        <p>
-            Valley View School 
-            <br></br>
-            Class 12 : 91.8%
-            <br></br>
-            2021-2023 
-        </p>
-        <p>
-        Valley View School 
-            <br></br>
-            Class 12 : 93.2%
-            <br></br>
-            2009-2021
-            
-        </p>
+    <div className="bg-primary p-6 rounded-lg flex flex-col">
+      <SectionTitle title="Education" />
+      <div className="flex flex-col gap-6 text-white py-6">
+        <div className="bg-gray-900 p-4 rounded-md">
+          <h3 className="text-xl font-bold text-yellow-400">Birla Institute of Technology</h3>
+          <p>B.Tech in CSE</p>
+          <p>CGPA: 9.04</p>
+          <p>September 2024 - 2027</p>
         </div>
+        <div className="bg-gray-900 p-4 rounded-md">
+          <p>Jee Main <strong className="text-yellow-400">AIR: 2440</strong></p>
+        </div>
+        <div className="bg-gray-900 p-4 rounded-md">
+          <h3 className="text-xl font-bold text-yellow-400">Valley View School</h3>
+          <p>Class 12: 91.8%</p>
+          <p>2021 - 2023</p>
+        </div>
+        <div className="bg-gray-900 p-4 rounded-md">
+          <h3 className="text-xl font-bold text-yellow-400">Valley View School</h3>
+          <p>Class 10: 93.2%</p>
+          <p>2009 - 2021</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Education
+export default Education;
