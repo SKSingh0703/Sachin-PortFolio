c2d7655c12ef5db2d43149c86aa65a09f250da93
Sachin Kumar <157154133+SKSingh0703@users.noreply.github.com>
2024-07-13 08:47:57 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,15 +10,6 @@ app.use(express.json());
 
 app.use("/api/portfolio", portfolioRoute);
 
-
-// const path = require("path");
-
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname,"client/build")));
-//     app.get("*",(req,res) => {
-//         res.sendFile(path.join(__dirname,"client/build/index.html"));
-//     });
-// }
 const port = process.env.PORT || 10000  ;
 app.listen(port, () => {
     console.log(`Server Listening on ${port}`);
