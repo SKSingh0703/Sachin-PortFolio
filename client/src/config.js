d3f1e14bfdcf664cdaca7bcc1e3360d1bde97967
Sachin Kumar <157154133+SKSingh0703@users.noreply.github.com>
2024-07-13 10:16:31 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,6 @@
+const API_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:5000/api/portfolio' // or whatever your local backend URL is
+  : 'https://sachin-portfolio-backend.onrender.com/api/portfolio';
+
+export default API_URL;
 
