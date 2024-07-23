e521868f06d8b28239f6e334ce09e4b6324e9fa9
Sachin Kumar <157154133+SKSingh0703@users.noreply.github.com>
2024-07-23 16:25:56 +0530

    Update vite.config.js

--- client/src/vite.config.js
+++ client/src/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://sachin-portfolio-backend.onrender.com',
         secure:false,
       },
     },
